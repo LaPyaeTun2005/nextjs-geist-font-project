3b4f098c48232ac0a6823049c92cda231f2bcd64
LaPyaeTun2005 <lapyaetun070@gmail.com>
2025-07-23 12:54:14 +0630

    Add .next/static/chunks/src_app_tournaments_page_tsx_17af3cd3._.js 1753251909724

---
+++ .next/static/chunks/src_app_tournaments_page_tsx_17af3cd3._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_464a2383._.js",
+  "static/chunks/src_291f8233._.js"
+],
+    source: "dynamic"
+});

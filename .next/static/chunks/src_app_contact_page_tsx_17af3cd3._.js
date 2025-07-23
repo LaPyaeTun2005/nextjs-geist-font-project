dfb0715b1a6663437d978f2d176259977b7ee6c4
LaPyaeTun2005 <lapyaetun070@gmail.com>
2025-07-23 12:54:09 +0630

    Add .next/static/chunks/src_app_contact_page_tsx_17af3cd3._.js 1753251904194

---
+++ .next/static/chunks/src_app_contact_page_tsx_17af3cd3._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_96603d45._.js",
+  "static/chunks/node_modules_1ceb61d5._.js"
+],
+    source: "dynamic"
+});
